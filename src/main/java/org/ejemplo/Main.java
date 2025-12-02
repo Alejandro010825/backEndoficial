bc2b63737aee1d7b6603964fd9b54f92b1395753
Alejandro010825 <251219@ids.upchiapas.edu.mx>
2025-12-02 14:04:37 -0600

    actualizacion

--- src/main/java/org/ejemplo/Main.java
+++ src/main/java/org/ejemplo/Main.java
@@ -4,6 +4,7 @@ import io.javalin.Javalin;
 import org.rutas.RepartoGraficaRutas;
 import org.rutas.*;
 
+
 public class Main {
     public static void main(String[] args) {
 
@@ -15,7 +16,7 @@ public class Main {
 
         app.get("/", ctx -> ctx.result("puerto 7001"));
 
-        // Registrar rutas
+
         AsistenciaRutas.registrar(app);
         ConfiguracionRutas.registrar(app);
         DestinoRutas.registrar(app);
--- src/main/java/org/project/db/DBConnection.java
+++ src/main/java/org/project/db/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/KungFu?serverTimezone=UTC&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/BACK?serverTimezone=UTC&useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
@@ -14,7 +14,7 @@ public class DBConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver MySQL cargado correctamente");
         } catch (ClassNotFoundException e) {
-            System.err.println("ERROR CRÍTICO: Driver MySQL NO encontrado");
+            System.err.println("ERROR");
             System.err.println("Verifica que mysql-connector-j esté en las dependencias");
             e.printStackTrace();
         }
@@ -24,7 +24,7 @@ public class DBConnection {
         System.out.println(" Intentando conectar a: " + URL);
         try {
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println(" Conexion exitosa a la base de datos KungFu");
+            System.out.println(" Conexion exitosa a la base de datos BACK");
             return conn;
         } catch (SQLException e) {
             System.err.println(" Error al conectar:");
